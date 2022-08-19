a2bb7b119267e8fbb4467d909b443bea2d70c96d
geoffreykithuku <mutembeikithuku12@gmail.com>
2022-08-19 16:55:31 +0300

    Completed assignment

---
+++ src/00-objectball.js
@@ -0,0 +1,129 @@
+
+let gameObject = () => {
+	const obj = {
+		home: {
+			teamName: "kamennu",
+			colors: ["Black", "White"],
+			players: {
+				"Black Rick": {
+					number: 0,
+					shoe: 16,
+					points: 22,
+					rebounds: 12,
+					assists: 12,
+					steals: 3,
+					blocks: 1,
+					slamDunks: 1
+				},
+				"Anderson may": {
+					number: 30,
+					shoe: 14,
+					points: 12,
+					rebounds: 12,
+					assists: 12,
+					steals: 12,
+					blocks: 12,
+					slamDunks: 7
+				},
+				"Geoffrey Miller": {
+					number: 11,
+					shoe: 17,
+					points: 17,
+					rebounds: 19,
+					assists: 10,
+					steals: 3,
+					blocks: 1,
+					slamDunks: 15
+				},
+				"Maingi Blue": {
+					number: 1,
+					shoe: 19,
+					points: 26,
+					rebounds: 12,
+					assists: 6,
+					steals: 3,
+					blocks: 8,
+					slamDunks: 5
+				},
+				"James Adre": {
+					number: 31,
+					shoe: 15,
+					points: 19,
+					rebounds: 2,
+					assists: 2,
+					steals: 4,
+					blocks: 11,
+					slamDunks: 1
+				}
+			}
+		},
+		away: {
+			teamName: "Harrabbe Stars",
+			colors: ["Turquoise", "Purple"],
+			players: {
+				"Mitley Bon": {
+					number: 4,
+					shoe: 18,
+					points: 10,
+					rebounds: 1,
+					assists: 1,
+					steals: 2,
+					blocks: 7,
+					slamDunks: 2
+				},
+				"Ashley Kile": {
+					number: 0,
+					shoe: 16,
+					points: 12,
+					rebounds: 4,
+					assists: 7,
+					steals: 7,
+					blocks: 15,
+					slamDunks: 10
+				},
+				"Disney white": {
+					number: 2,
+					shoe: 14,
+					points: 24,
+					rebounds: 12,
+					assists: 12,
+					steals: 4,
+					blocks: 5,
+					slamDunks: 5
+				},
+				"Jamlec Doba": {
+					number: 8,
+					shoe: 15,
+					points: 33,
+					rebounds: 3,
+					assists: 2,
+					steals: 1,
+					blocks: 1,
+					slamDunks: 0
+				},
+				"willy scott": {
+					number: 33,
+					shoe: 15,
+					points: 6,
+					rebounds: 12,
+					assists: 12,
+					steals: 22,
+					blocks: 5,
+					slamDunks: 12
+				}
+			}
+		},
+	}
+	return obj;
+}
+
+let homeTeamName = () => {
+	let object = gameObject()
+	return object['home']['teamName']
+}
+
+let awayTeamName = () => {
+	let object = gameObject()
+	return object['away']['teamName']
+}
+
